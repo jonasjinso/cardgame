61b40a2dd8b08a20d3a5578e9fe91c9628c842a0
bbm3h15apr <bbm3h15apr@PC08-05.bg.bib.de>
2018-03-05 12:17:41 +0100

    efef

--- CardGame/src/Controller/Controller.java
+++ CardGame/src/Controller/Controller.java
@@ -2,9 +2,11 @@ package Controller;
 
 import javax.swing.text.View;
 
+import View.Launcher;
 import View.Login;
 
 public class Controller {
 
 	private Login l = new Login();
+	private Launcher lau = new Launcher();
 }
---
+++ CardGame/src/View/Launcher.java
@@ -0,0 +1,11 @@
+package View;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Launcher extends JFrame{
+
+	private Container c = getContentPane();
+	
+}

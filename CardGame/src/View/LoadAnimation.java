6c0e14ca284329c2e2d3b2e42d3232a243e32061
BKRob <BKRob>
2018-03-08 13:07:18 +0100

    screen

--- CardGame/src/View/LoadAnimation.java
+++ CardGame/src/View/LoadAnimation.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class LoadAnimation extends JPanel implements Runnable {
@@ -11,6 +12,7 @@ public class LoadAnimation extends JPanel implements Runnable {
 	private int hoehe;
 
 	private Image img;
+	
 
 	public Dimension getPreferredSize(){
 		return new Dimension(breite, hoehe);
@@ -23,40 +25,69 @@ public class LoadAnimation extends JPanel implements Runnable {
 	}
 
 	public LoadAnimation(){
-		
+		setBackground(Color.BLACK);
 
 	}
 	
 	private int i = 0;
+	private boolean b = false;
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-
-		/*//img.setImage(img.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
-		BufferedImage bi = new BufferedImage(img.getIconWidth(), img.getIconHeight()
-				, BufferedImage.TYPE_INT_ARGB);
 		
-		AffineTransform at = AffineTransform.getTranslateInstance(100, 100);
-		at.rotate(Math.toRadians(i++), img.getIconWidth()/2, img.getIconHeight()/2);
+		g.setColor(Color.WHITE);
+		g.fillRect(300, 180, 800, 10);
 		
-		Graphics2D g2d = (Graphics2D) g;
+		g.setColor(Color.WHITE);
+		g.fillRect(300, 340, 800, 10);
 		
-		g2d.drawImage(bi, at, null);*/
+		g.setColor(Color.WHITE);
+		g.fillRect(1100, 180, 10, 170);
+		
+		//g.setColor(Color.RED);
+		//g.fillRect(380, 200, 40, 130);
+		
+		if(b == true){
+			g.setColor(Color.RED);
+			g.fillRect(380, 200, 40, 130);
+			x++;
+			b = false;
+		}
+		
 		
 		img = getToolkit().getImage("logoHearthstone_Rock_HEwn.png");
 		
 		AffineTransform at = AffineTransform.getTranslateInstance(100, 100);
-		at.rotate(Math.toRadians(i++), 100,100);
+		at.rotate(Math.toRadians(i++), img.getWidth(this)/2, img.getHeight(this)/2);
 		
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.drawImage(img, at, this);
 		
+		
+		
+		
+		
 		repaint();
 	}
+	
+	private int x = 0;
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		while(x != 10){
+			
+			if(b == false){
+				try {
+					Thread.sleep(1000);
+					b = true;
+					repaint();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
 		
 	}
 

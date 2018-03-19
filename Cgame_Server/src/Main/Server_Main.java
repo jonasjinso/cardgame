655d8467f58056bcca45cdb43a399dcb59ab78d4
BKRob <BKRob>
2018-03-19 12:28:48 +0100

    www

--- Cgame_Server/src/Main/Server_Main.java
+++ Cgame_Server/src/Main/Server_Main.java
@@ -1,9 +1,13 @@
 package Main;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
 public class Server_Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws RemoteException {
+		
+		LocateRegistry.createRegistry(7777);
 
 	}
 

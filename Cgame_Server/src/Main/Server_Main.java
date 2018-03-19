34b34dee0dddb4895313043eb45cc3ba20f88186
BKRob <BKRob>
2018-03-19 12:36:59 +0100

    sss

--- Cgame_Server/src/Main/Server_Main.java
+++ Cgame_Server/src/Main/Server_Main.java
@@ -1,16 +1,20 @@
 package Main;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
 public class Server_Main {
 
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws RemoteException, MalformedURLException {
 		
 		LocateRegistry.createRegistry(7777);
 		
-		
-
+		Remote remote = (Remote) new Model();
+		Naming.rebind("//localhost:7777/game", remote);
+		System.out.println("Server start...");
 	}
 
 }
